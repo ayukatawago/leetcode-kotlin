6c9ad1e93b05a645ee9dca95df2d34da69116680
ayukatawago <ayukatawago7@gmail.com>
2022-02-07 09:56:00 +0900

    1. Two Sum

---
+++ src/main/kotlin/0001-two-sum/Solution0001.kt
@@ -0,0 +1,16 @@
+class Solution0001 {
+    fun twoSum(nums: IntArray, target: Int): IntArray? {
+        val hashmap = HashMap<Int, Int>()
+
+        nums.forEachIndexed { index, value ->
+            val missing = target - value
+            if (hashmap.containsKey(missing) && hashmap[missing] != index) {
+                hashmap[missing]?.let {
+                    return intArrayOf(it, index)
+                }
+            }
+            hashmap[value]= index
+        }
+        return null
+    }
+}
