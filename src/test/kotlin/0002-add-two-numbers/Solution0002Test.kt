6e814e21f686ede59174a1a3ff7abc028ea92e68
Takuya Ogawa <46732412+ayukatawago@users.noreply.github.com>
2022-03-22 05:56:59 +0900

    2. Add Two Numbers

---
+++ src/test/kotlin/0002-add-two-numbers/Solution0002Test.kt
@@ -0,0 +1,78 @@
+import `0002-add-two-numbers`.ListNode
+import `0002-add-two-numbers`.Solution0002
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Solution0002Test {
+    private val target = Solution0002()
+
+    @Test
+    fun test1() {
+        val input1 = listOf(2, 4, 3)
+        val input2 = listOf(5, 6, 4)
+        val output = listOf(7, 0, 8)
+        exec(input1, input2, output)
+    }
+
+    @Test
+    fun test2() {
+        val input1 = listOf(0)
+        val input2 = listOf(0)
+        val output = listOf(0)
+        exec(input1, input2, output)
+    }
+
+    @Test
+    fun test3() {
+        val input1 = listOf(9, 9, 9, 9, 9, 9, 9)
+        val input2 = listOf(9, 9, 9, 9)
+        val output = listOf(8, 9, 9, 9, 0, 0, 0, 1)
+        exec(input1, input2, output)
+    }
+
+    @Test
+    fun test4() {
+        val input1 = listOf(9)
+        val input2 = listOf(1, 9, 9, 9, 9, 9, 9, 9, 9, 9)
+        val output = listOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1)
+        exec(input1, input2, output)
+    }
+
+    @Test
+    fun test5() {
+        val input1 = listOf(8, 3, 2)
+        val input2 = listOf(9, 2, 1)
+        val output = listOf(7, 6, 3)
+        exec(input1, input2, output)
+    }
+
+    private fun exec(input1: List<Int>, input2: List<Int>, output: List<Int>) {
+        val listNode1 = input1.toListNode()
+        val listNode2 = input2.toListNode()
+        var result = target.addTwoNumbers(listNode1, listNode2)
+        output.forEach {
+            assertEquals(it, result?.`val`)
+            result = result?.next
+        }
+    }
+
+    private fun List<Int>.toListNode(): ListNode? {
+        if (size == 0) {
+            return null
+        }
+        val head = ListNode(get(0))
+        drop(1).forEach {
+            head.append(it)
+        }
+
+        return head
+    }
+
+    private fun ListNode.append(value: Int) {
+        if (next != null) {
+            next!!.append(value)
+        } else {
+            next = ListNode(value)
+        }
+    }
+}
