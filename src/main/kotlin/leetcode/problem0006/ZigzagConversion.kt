6b114a0bca806b945d43c6a06b13b3a42a3cd1df
ayukatawago <ayukatawago7@gmail.com>
2022-03-21 10:45:27 +0900

    6. Zigzag Conversion

---
+++ src/main/kotlin/leetcode/problem0006/ZigzagConversion.kt
@@ -0,0 +1,25 @@
+package leetcode.problem0006
+
+class ZigzagConversion {
+    fun convert(s: String, numRows: Int): String {
+        if (numRows == 1) {
+            return s
+        }
+
+        val zigzagSize = numRows * 2 - 2
+        val zigzagStringArray: Array<MutableList<Char>> = Array(numRows) { mutableListOf() }
+        val zigzagIndexArray = Array(zigzagSize) { 0 }
+        for(row in 0 until numRows) {
+            zigzagIndexArray[row] = row
+            if (row != 0 && row != numRows -1) {
+                zigzagIndexArray[zigzagSize - row] = row
+            }
+        }
+        s.forEachIndexed { index, c ->
+            val zigzagIndex = zigzagIndexArray[index % zigzagSize]
+            zigzagStringArray[zigzagIndex].add(c)
+        }
+
+        return zigzagStringArray.flatMap { it }.joinToString("")
+    }
+}
