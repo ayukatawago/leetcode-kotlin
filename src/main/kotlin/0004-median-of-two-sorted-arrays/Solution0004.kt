2abd779eaea3f9566ab132196eaeb0825ca85e06
Takuya Ogawa <46732412+ayukatawago@users.noreply.github.com>
2022-03-22 05:57:26 +0900

    4. Median of Two Sorted Arrays

---
+++ src/main/kotlin/0004-median-of-two-sorted-arrays/Solution0004.kt
@@ -0,0 +1,27 @@
+package `0004-median-of-two-sorted-arrays`
+
+class Solution0004 {
+    fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
+        val length = nums1.size + nums2.size
+        val shouldHandleEvenArray = length % 2 == 0
+        val median = length / 2
+        val mergedIntArray = IntArray(median + 1) { 0 }
+        var pointer1 = 0
+        var pointer2 = 0
+        var index = 0
+
+        while (index < median + 1) {
+            mergedIntArray[index++] = when {
+                pointer1 == nums1.size -> nums2[pointer2++]
+                pointer2 == nums2.size -> nums1[pointer1++]
+                nums1[pointer1] <= nums2[pointer2] -> nums1[pointer1++]
+                else -> nums2[pointer2++]
+            }
+        }
+        return if(shouldHandleEvenArray) {
+            (mergedIntArray[median - 1].toDouble() + mergedIntArray[median].toDouble()) / 2
+        } else {
+            mergedIntArray[median].toDouble()
+        }
+    }
+}
