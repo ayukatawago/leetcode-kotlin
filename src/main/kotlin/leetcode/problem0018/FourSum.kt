2750484b090266e8ee721c9f73a5ca56fc7fe1bd
Takuya Ogawa <46732412+ayukatawago@users.noreply.github.com>
2022-06-06 12:32:28 +0900

    18. 4 Sum

---
+++ src/main/kotlin/leetcode/problem0018/FourSum.kt
@@ -0,0 +1,61 @@
+package leetcode.problem0018
+
+class FourSum {
+    fun fourSum(nums: IntArray, target: Int): List<List<Int>> {
+        val sortedNums = nums.sorted()
+
+        return kSum(sortedNums, target, 4).toSet().toList()
+    }
+
+    private fun kSum(sortedNums: List<Int>, target: Int, k: Int): List<List<Int>> {
+        if (sortedNums.isEmpty()) {
+            return emptyList()
+        }
+        val answerMap = HashMap<Int, List<List<Int>>>()
+        sortedNums.forEachIndexed { index, num ->
+            if (answerMap[num] != null) {
+                return@forEachIndexed
+            }
+            val remainingNums = sortedNums.drop(index + 1)
+            val answerList = if (k == 3) {
+                twoSum(remainingNums, target - num).map { it.toAnswer(num) }
+            } else {
+                kSum(remainingNums, target - num, k - 1).map { it.toAnswer(num) }
+            }
+            answerMap[num] = answerList
+        }
+        return answerMap.flatMap { it.value }
+    }
+
+    private fun List<Int>.toAnswer(num: Int): List<Int> {
+        val answerList = mutableListOf(num)
+        forEach {
+            answerList.add(it)
+        }
+        return answerList
+    }
+
+    private fun twoSum(sortedNums: List<Int>, target: Int): List<List<Int>> {
+        if (sortedNums.size < 2) {
+            return emptyList()
+        }
+
+        val answerList = mutableListOf<List<Int>>()
+        var left = 0
+        var right = sortedNums.lastIndex
+
+        while (left < right) {
+            val leftValue = sortedNums[left]
+            val rightValue = sortedNums[right]
+            when {
+                leftValue + rightValue == target -> {
+                    answerList.add(listOf(leftValue, rightValue))
+                    left += 1
+                }
+                leftValue + rightValue < target -> left += 1
+                leftValue + rightValue > target -> right -= 1
+            }
+        }
+        return answerList
+    }
+}
