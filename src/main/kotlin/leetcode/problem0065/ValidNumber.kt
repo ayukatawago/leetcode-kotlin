32dc42b594ec9262298f432b720b5c94f0477f54
Takuya Ogawa <46732412+ayukatawago@users.noreply.github.com>
2022-08-13 06:29:00 +0900

    65. Valid Number
    Solution without Regular Expressions

--- src/main/kotlin/leetcode/problem0065/ValidNumber.kt
+++ src/main/kotlin/leetcode/problem0065/ValidNumber.kt
@@ -2,7 +2,41 @@ package leetcode.problem0065
 
 class ValidNumber {
     fun isNumber(s: String): Boolean {
-        val regex = Regex("""[+-]?(?:\d+\.?\d*|\.\d+)(?:[eE][+-]?\d+)?""")
-        return s.matches(regex)
+        var hasDigit = false
+        var hasDecimal = false
+        var hasExponent = false
+        var shouldHaveDigit = false
+
+        s.forEachIndexed { index, c ->
+            when {
+                c in listOf('+', '-') -> {
+                    if (index != 0 && !s[index - 1].isExponentChar()) {
+                        return false
+                    }
+                }
+                c.isDigit() -> {
+                    hasDigit = true
+                    shouldHaveDigit = false
+                }
+                c == '.' -> {
+                    if (hasDecimal || hasExponent) {
+                        return false
+                    }
+                    hasDecimal = true
+                }
+                c.isExponentChar() -> {
+                    if (hasExponent || !hasDigit) {
+                        return false
+                    }
+                    hasExponent = true
+                    shouldHaveDigit = true
+                }
+                else -> return false
+            }
+        }
+
+        return !shouldHaveDigit && hasDigit
     }
+
+    private fun Char.isExponentChar(): Boolean = this in listOf('E', 'e')
 }
