73dc55960b549a3ebcbba11110579183d677ef75
Takuya Ogawa <46732412+ayukatawago@users.noreply.github.com>
2022-08-16 06:35:52 +0900

    Faster solution

--- src/main/kotlin/leetcode/problem0030/SubstringWithConcatenationOfAllWords.kt
+++ src/main/kotlin/leetcode/problem0030/SubstringWithConcatenationOfAllWords.kt
@@ -4,15 +4,27 @@ class SubstringWithConcatenationOfAllWords {
     fun findSubstring(s: String, words: Array<String>): List<Int> {
         val wordSize = words[0].length
         val windowSize = wordSize * words.size
+        val wordsMap = hashMapOf<String, Int>()
+        words.forEach {
+            wordsMap[it] = (wordsMap[it] ?: 0) + 1
+        }
 
         val answer = mutableListOf<Int>()
         s.indices.forEach { index ->
             if (s.length - index < windowSize) {
                 return@forEach
             }
-            val window = s.slice(index until index + windowSize)
-            val wordsInWindow = window.chunked(wordSize).sorted()
-            if (words.sorted() == wordsInWindow) {
+            var window = s.slice(index until index + windowSize)
+            val foundMap = hashMapOf<String, Int>()
+            while (window.length >= wordSize) {
+                val word = window.substring(0, wordSize)
+                if (word !in wordsMap.keys) {
+                    break
+                }
+                foundMap[word] = (foundMap[word] ?: 0) + 1
+                window = window.substring(wordSize)
+            }
+            if (foundMap == wordsMap) {
                 answer.add(index)
             }
         }
