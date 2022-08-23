c3889fd3104512ae4e883995c71cd3d26cbbbee3
Takuya Ogawa <46732412+ayukatawago@users.noreply.github.com>
2022-08-24 08:14:08 +0900

    36. Valid Sudoku

---
+++ src/test/kotlin/leetcode/problem0036/ValidSudokuTest.kt
@@ -0,0 +1,41 @@
+package leetcode.problem0036
+
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+internal class ValidSudokuTest {
+    private val target = ValidSudoku()
+
+    @Test
+    fun test1() {
+        val board = arrayOf(
+            charArrayOf('5', '3', '.', '.', '7', '.', '.', '.', '.'),
+            charArrayOf('6', '.', '.', '1', '9', '5', '.', '.', '.'),
+            charArrayOf('.', '9', '8', '.', '.', '.', '.', '6', '.'),
+            charArrayOf('8', '.', '.', '.', '6', '.', '.', '.', '3'),
+            charArrayOf('4', '.', '.', '8', '.', '3', '.', '.', '1'),
+            charArrayOf('7', '.', '.', '.', '2', '.', '.', '.', '6'),
+            charArrayOf('.', '6', '.', '.', '.', '.', '2', '8', '.'),
+            charArrayOf('.', '.', '.', '4', '1', '9', '.', '.', '5'),
+            charArrayOf('.', '.', '.', '.', '8', '.', '.', '7', '9'),
+        )
+        assertTrue(target.isValidSudoku(board))
+    }
+
+    @Test
+    fun test2() {
+        val board = arrayOf(
+            charArrayOf('8', '3', '.', '.', '7', '.', '.', '.', '.'),
+            charArrayOf('6', '.', '.', '1', '9', '5', '.', '.', '.'),
+            charArrayOf('.', '9', '8', '.', '.', '.', '.', '6', '.'),
+            charArrayOf('8', '.', '.', '.', '6', '.', '.', '.', '3'),
+            charArrayOf('4', '.', '.', '8', '.', '3', '.', '.', '1'),
+            charArrayOf('7', '.', '.', '.', '2', '.', '.', '.', '6'),
+            charArrayOf('.', '6', '.', '.', '.', '.', '2', '8', '.'),
+            charArrayOf('.', '.', '.', '4', '1', '9', '.', '.', '5'),
+            charArrayOf('.', '.', '.', '.', '8', '.', '.', '7', '9'),
+        )
+        assertFalse(target.isValidSudoku(board))
+    }
+}
