39377a00b46e0cabde5600fb57649e8d3a3d3210
Takuya Ogawa <46732412+ayukatawago@users.noreply.github.com>
2022-08-24 08:28:37 +0900

    Solution using HashSet

--- src/main/kotlin/leetcode/problem0036/ValidSudoku.kt
+++ src/main/kotlin/leetcode/problem0036/ValidSudoku.kt
@@ -6,59 +6,23 @@ class ValidSudoku {
             return false
         }
 
-        // Check column
-        board.forEach forEachRow@{ row ->
-            if (!hasValidList(row.toList())) {
-                return false
-            }
-        }
+        val hashSet = HashSet<String>()
 
-        // Check row
-        board[0].indices.forEach { index ->
-            val column = board.map { it[index] }
-            if (!hasValidList(column)) {
-                return false
-            }
-        }
+        board.forEachIndexed { rowIndex, row ->
+            row.forEachIndexed forEachColumn@{ columnIndex, num ->
+                if (num == '.') {
+                    return@forEachColumn
+                }
 
-        // Check box
-        BOX_INDEX.forEach { row ->
-            BOX_INDEX.forEach { column ->
-                val box = listOf(
-                    board[row][column],
-                    board[row][column + 1],
-                    board[row][column + 2],
-                    board[row + 1][column],
-                    board[row + 1][column + 1],
-                    board[row + 1][column + 2],
-                    board[row + 2][column],
-                    board[row + 2][column + 1],
-                    board[row + 2][column + 2]
-                )
-                if (!hasValidList(box)) {
+                if (!hashSet.add("$num in row $rowIndex") ||
+                    !hashSet.add("$num in column $columnIndex") ||
+                    !hashSet.add("$num in box ${rowIndex / 3}, ${columnIndex / 3}")
+                ) {
                     return false
                 }
             }
         }
 
         return true
-    }
-
-    private fun hasValidList(input: List<Char>): Boolean {
-        val tmpSet = mutableSetOf<Char>()
-        input.forEach {
-            if (it == '.') {
-                return@forEach
-            }
-            if (it in tmpSet) {
-                return false
-            }
-            tmpSet.add(it)
-        }
-        return true
-    }
-
-    companion object {
-        private val BOX_INDEX = listOf(0, 3, 6)
     }
 }
