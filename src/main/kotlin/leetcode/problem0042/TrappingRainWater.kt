8f62f01aa97f87bf9803366f6bc98395005a9618
Takuya Ogawa <46732412+ayukatawago@users.noreply.github.com>
2022-08-31 10:19:24 +0900

    42. Trapping Rain Water
    Solution using stack

--- src/main/kotlin/leetcode/problem0042/TrappingRainWater.kt
+++ src/main/kotlin/leetcode/problem0042/TrappingRainWater.kt
@@ -1,28 +1,24 @@
 package leetcode.problem0042
 
+import java.util.Stack
+import kotlin.math.min
+
 class TrappingRainWater {
     fun trap(height: IntArray): Int {
-        var left = 0
-        var right = height.lastIndex
         var answer = 0
-        var leftMax = 0
-        var rightMax = 0
-        while (left < right) {
-            if (height[left] < height[right]) {
-                if (height[left] >= leftMax) {
-                    leftMax = height[left]
-                } else {
-                    answer += leftMax - height[left]
+        val stack = Stack<Int>()
+        height.forEachIndexed { index, i ->
+            while (stack.isNotEmpty() && i > height[stack.peek()]) {
+                val bottomIndex = stack.pop()
+                if (stack.isEmpty()) {
+                    break
                 }
-                left++
-            } else {
-                if (height[right] >= rightMax) {
-                    rightMax = height[right]
-                } else {
-                    answer += rightMax - height[right]
-                }
-                right--
+
+                val distance = index - stack.peek() - 1
+                val boundedHeight = min(i, height[stack.peek()]) - height[bottomIndex]
+                answer += distance * boundedHeight
             }
+            stack.push(index)
         }
 
         return answer
