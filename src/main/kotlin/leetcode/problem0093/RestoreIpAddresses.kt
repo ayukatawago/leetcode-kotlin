b1d759532d26b5b0b8aea0ad6b116686c9fb5144
Takuya Ogawa <46732412+ayukatawago@users.noreply.github.com>
2022-11-04 00:10:07 +0900

    93. Restore IP Addresses
    Improved solution

--- src/main/kotlin/leetcode/problem0093/RestoreIpAddresses.kt
+++ src/main/kotlin/leetcode/problem0093/RestoreIpAddresses.kt
@@ -2,46 +2,38 @@ package leetcode.problem0093
 
 class RestoreIpAddresses {
     fun restoreIpAddresses(s: String): List<String> {
-        return getCandidatesSet(s).filter { it.size == 4 }.map { it.joinToString(".") }
+        return getCandidatesSet(s, 0, 0).map { it.joinToString(".") }
     }
 
-    private fun getCandidatesSet(s: String): Set<List<String>> {
-        if (s.isEmpty()) {
-            return emptySet()
-        }
-        if (s.length == 1) {
-            return setOf(listOf(s))
-        }
-        if (s.length == 2) {
-            return if (s.startsWith('0')) {
-                setOf(listOf(s[0].toString(), s[1].toString()))
+    private fun getCandidatesSet(
+        s: String,
+        index: Int,
+        level: Int
+    ): Set<List<String>> {
+        if (level == 3) {
+            val num = s.slice(index..s.lastIndex)
+            return if (num.hasValidValue()) {
+                setOf(listOf(num))
             } else {
-                setOf(listOf(s[0].toString(), s[1].toString()), listOf(s))
+                emptySet()
             }
         }
 
-        if (s.startsWith('0')) {
-            return createCandidateSet(s, 1)
+        val answerSet = mutableSetOf<List<String>>()
+        (1..3).forEach { offset ->
+            if (index + offset > s.length) return@forEach
+
+            val num = s.slice(index until index + offset)
+            if (num.hasValidValue()) {
+                val candidates = getCandidatesSet(s, index + offset, level + 1)
+                    .filter { it.isNotEmpty() }
+                    .map { listOf(num) + it }
+                answerSet.addAll(candidates)
+            }
         }
-
-        val answer = if (s.length == 3 && s.toInt() <= 255) {
-            mutableSetOf(listOf(s))
-        } else {
-            mutableSetOf()
-        }
-
-        if (s.slice(0..2).toInt() <= 255) {
-            answer += createCandidateSet(s, 3)
-        }
-
-        answer += createCandidateSet(s, 2)
-        answer += createCandidateSet(s, 1)
-
-        return answer
+        return answerSet
     }
 
-    private fun createCandidateSet(s: String, count: Int): Set<List<String>> =
-        getCandidatesSet(s.drop(count)).filter { it.size < 4 }.map { candidate ->
-            listOf(s.slice(0 until count)) + candidate
-        }.toSet()
+    private fun String.hasValidValue(): Boolean =
+        length == 1 || length == 2 && (toInt() in 10..99) || length == 3 && (toInt() in 100..255)
 }
