b02c717aacd146b118bd955e5da07c9b9b2eada8
Takuya Ogawa <46732412+ayukatawago@users.noreply.github.com>
2022-11-22 14:35:43 +0900

    102. Binary Tree Level Order Traversal

---
+++ src/test/kotlin/leetcode/problem0102/BinaryTreeLevelOrderTraversalTest.kt
@@ -0,0 +1,45 @@
+package leetcode.problem0102
+
+import leetcode.shared.TreeNode
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+internal class BinaryTreeLevelOrderTraversalTest {
+    private val target = BinaryTreeLevelOrderTraversal()
+
+    @Test
+    fun test1() {
+        val root = TreeNode.from(arrayOf(3, 9, 20, null, null, 15, 7))
+        assertEquals(
+            listOf(listOf(3), listOf(9, 20), listOf(15, 7)),
+            target.levelOrder(root)
+        )
+    }
+
+    @Test
+    fun test2() {
+        val root = TreeNode.from(arrayOf(1))
+        assertEquals(
+            listOf(listOf(1)),
+            target.levelOrder(root)
+        )
+    }
+
+    @Test
+    fun test3() {
+        val root = TreeNode.from(arrayOf())
+        assertEquals(
+            listOf(),
+            target.levelOrder(root)
+        )
+    }
+
+    @Test
+    fun test4() {
+        val root = TreeNode.from(arrayOf(1, 2, 3, 4, 5))
+        assertEquals(
+            listOf(listOf(1), listOf(2, 3), listOf(4, 5)),
+            target.levelOrder(root)
+        )
+    }
+}
