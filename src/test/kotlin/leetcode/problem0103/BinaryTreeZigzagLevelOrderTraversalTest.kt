5680d3d64f172c48f15978ff4a8e79195ce0dd2a
Takuya Ogawa <46732412+ayukatawago@users.noreply.github.com>
2022-11-24 14:56:01 +0900

    103. Binary Tree Zigzag Level Order Traversal

---
+++ src/test/kotlin/leetcode/problem0103/BinaryTreeZigzagLevelOrderTraversalTest.kt
@@ -0,0 +1,54 @@
+package leetcode.problem0103
+
+import leetcode.shared.TreeNode
+import org.junit.Test
+import kotlin.test.assertEquals
+
+internal class BinaryTreeZigzagLevelOrderTraversalTest {
+    private val target = BinaryTreeZigzagLevelOrderTraversal()
+
+    @Test
+    fun test1() {
+        val root = TreeNode.from(arrayOf(3, 9, 20, null, null, 15, 7))
+        assertEquals(
+            listOf(listOf(3), listOf(20, 9), listOf(15, 7)),
+            target.zigzagLevelOrder(root)
+        )
+    }
+
+    @Test
+    fun test2() {
+        val root = TreeNode.from(arrayOf(1))
+        assertEquals(
+            listOf(listOf(1)),
+            target.zigzagLevelOrder(root)
+        )
+    }
+
+    @Test
+    fun test3() {
+        val root = TreeNode.from(arrayOf())
+        assertEquals(
+            listOf(),
+            target.zigzagLevelOrder(root)
+        )
+    }
+
+    @Test
+    fun test4() {
+        val root = TreeNode.from(arrayOf(1, 2, 3, 4, 5))
+        assertEquals(
+            listOf(listOf(1), listOf(3, 2), listOf(4, 5)),
+            target.zigzagLevelOrder(root)
+        )
+    }
+
+    @Test
+    fun test5() {
+        val root = TreeNode.from(arrayOf(1, 2, 3, 4, null, null, 5))
+        assertEquals(
+            listOf(listOf(1), listOf(3, 2), listOf(4, 5)),
+            target.zigzagLevelOrder(root)
+        )
+    }
+}
