cf1f94dae53dbf745a3df08859cf63f63729e0cf
Takuya Ogawa <46732412+ayukatawago@users.noreply.github.com>
2022-11-27 10:39:01 +0900

    104. Maximum Depth of Binary Tree

---
+++ src/test/kotlin/leetcode/problem0104/MaximumDepthOfBinaryTreeTest.kt
@@ -0,0 +1,21 @@
+package leetcode.problem0104
+
+import leetcode.shared.TreeNode
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+internal class MaximumDepthOfBinaryTreeTest {
+    private val target = MaximumDepthOfBinaryTree()
+
+    @Test
+    fun test1() {
+        val root = TreeNode.from(arrayOf(3, 9, 20, null, null, 15, 7))
+        assertEquals(3, target.maxDepth(root))
+    }
+
+    @Test
+    fun test2() {
+        val root = TreeNode.from(arrayOf(1, null, 2))
+        assertEquals(2, target.maxDepth(root))
+    }
+}
