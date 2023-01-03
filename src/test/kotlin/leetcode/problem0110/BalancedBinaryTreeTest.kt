72345d7d94efc43ea413f719f04a46e65c5ca02b
Takuya Ogawa <46732412+ayukatawago@users.noreply.github.com>
2023-01-03 22:01:31 +0900

    110. Balanced Binary Tree

---
+++ src/test/kotlin/leetcode/problem0110/BalancedBinaryTreeTest.kt
@@ -0,0 +1,34 @@
+package leetcode.problem0110
+
+import leetcode.shared.TreeNode
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class BalancedBinaryTreeTest {
+    private val target = BalancedBinaryTree()
+
+    @Test
+    fun test1() {
+        val root = TreeNode.from(arrayOf(3, 9, 20, null, null, 15, 7))
+        assertTrue(target.isBalanced(root))
+    }
+
+    @Test
+    fun test2() {
+        val root = TreeNode.from(arrayOf(1, 2, 2, 3, 3, null, null, 4, 4))
+        assertFalse(target.isBalanced(root))
+    }
+
+    @Test
+    fun test3() {
+        val root = TreeNode.from(emptyArray())
+        assertTrue(target.isBalanced(root))
+    }
+
+    @Test
+    fun test4() {
+        val root = TreeNode.from(arrayOf(1, 2, 2, 3, null, null, 3, 4, null, null, 4))
+        assertFalse(target.isBalanced(root))
+    }
+}
