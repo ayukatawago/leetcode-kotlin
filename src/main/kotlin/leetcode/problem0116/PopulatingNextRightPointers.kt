cfcab66a6fb531581b9aba787446015c6c7854a2
Takuya Ogawa <46732412+ayukatawago@users.noreply.github.com>
2023-01-23 11:31:55 +0900

    Update shared Node class to support null

--- src/main/kotlin/leetcode/problem0116/PopulatingNextRightPointers.kt
+++ src/main/kotlin/leetcode/problem0116/PopulatingNextRightPointers.kt
@@ -1,5 +1,7 @@
 package leetcode.problem0116
 
+import leetcode.shared.Node
+
 class PopulatingNextRightPointers {
     fun connect(root: Node?): Node? {
         if (root == null) {
@@ -19,40 +21,5 @@ class PopulatingNextRightPointers {
         }
 
         return root
-    }
-}
-
-class Node(var `val`: Int) {
-    var left: Node? = null
-    var right: Node? = null
-    var next: Node? = null
-
-    private fun insert(index: Int, input: Array<Int>) {
-        if (2 * index + 1 < input.size) {
-            input[2 * index + 1].let {
-                left = Node(it).also { node ->
-                    node.insert(2 * index + 1, input)
-                }
-            }
-        }
-        if (2 * index + 2 < input.size) {
-            input[2 * index + 2].let {
-                right = Node(it).also { node ->
-                    node.insert(2 * index + 2, input)
-                }
-            }
-        }
-    }
-
-    companion object {
-        fun from(input: Array<Int>): Node? {
-            if (input.isEmpty()) {
-                return null
-            }
-            val first = input[0]
-            val rootNode = Node(first)
-            rootNode.insert(0, input)
-            return rootNode
-        }
     }
 }
--- src/test/kotlin/leetcode/problem0116/PopulatingNextRightPointersTest.kt
+++ src/test/kotlin/leetcode/problem0116/PopulatingNextRightPointersTest.kt
@@ -1,5 +1,6 @@
 package leetcode.problem0116
 
+import leetcode.shared.Node
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -8,7 +9,7 @@ internal class PopulatingNextRightPointersTest {
 
     @Test
     fun test1() {
-        val root = arrayOf(1, 2, 3, 4, 5, 6, 7)
+        val root: Array<Int?> = arrayOf(1, 2, 3, 4, 5, 6, 7)
         val node = Node.from(root)
         assertEquals(
             listOf(1, '#', 2, 3, '#', 4, 5, 6, 7, '#'),
@@ -18,7 +19,7 @@ internal class PopulatingNextRightPointersTest {
 
     @Test
     fun test2() {
-        val root = arrayOf<Int>()
+        val root = arrayOf<Int?>()
         val node = Node.from(root)
         assertEquals(
             listOf(),
@@ -28,27 +29,11 @@ internal class PopulatingNextRightPointersTest {
 
     @Test
     fun test3() {
-        val root = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
+        val root: Array<Int?> = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
         val node = Node.from(root)
         assertEquals(
             listOf(1, '#', 2, 3, '#', 4, 5, 6, 7, '#', 8, 9, 10, 11, 12, 13, 14, 15, '#'),
             target.connect(node)?.toList()
         )
-    }
-
-    private fun Node.toList(): List<Any> {
-        val mutableList = mutableListOf<Any>()
-        var leftNode: Node? = this
-        while (leftNode != null) {
-            var node: Node? = leftNode
-            while (node != null) {
-                mutableList.add(node.`val`)
-                node = node.next
-            }
-            mutableList.add('#')
-            leftNode = leftNode.left
-        }
-
-        return mutableList
     }
 }
